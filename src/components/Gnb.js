245713c798ff7206f3b3b163ed14c120b58e1bef
munjs <mjcah2013@gmail.com>
2022-08-06 20:43:50 +0900

    menu

--- src/components/Gnb.js
+++ src/components/Gnb.js
@@ -6,9 +6,54 @@ const Gnb = () => {
     return (
         <div className="gnb">
             <div className="menu">
-                
+                <div className="login">
+                    <ul className="login_list">
+                        <li>
+                            <a>
+                                <span>
+                                login
+                                </span>
+                            </a>
+                        </li>
+                        <li>
+                            <a>
+                                <span>
+                                join
+                                </span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div className="menu_list">
+                    <ul className="list">
+                        <li>
+                            <a>
+                                <span>
+                            FLAVOR OF THE MONTH
+                            </span>
+                            </a>
+                        </li>
+                        <li>
+                        <a>
+                            <span>MENU</span>
+                        </a>
+                        </li>
+                        <li>
+                        <a><span>영양 성분 및 알레르기</span></a>
+                        </li>
+                        <li>
+                        <a><span>EVENT</span></a>
+                        </li>
+                        <li>
+                        <a><span>STORE</span></a>
+                        </li>
+                        <li>
+                        <a><span>ABOUT</span></a>
+                        </li>
+                    </ul>
+                </div>
             </div>
-            <div className="menu_list">
+            <div className="menu_item">
                 <img src={point} />
                 <img src={best} />
             </div>
